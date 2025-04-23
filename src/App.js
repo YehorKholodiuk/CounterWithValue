db4e6f15e417d23bb5338bc35aeb3b37644896be
ASUS <Yehor Kholodiuk>
2025-04-23 11:58:11 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(1)
+
+  const changeCount = (value) => {
+
+    setCount(count + value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>
+        <button onClick = {() => changeCount(1)}></button>
+        {count}
+        <button onClick = {() => changeCount(-1)}></button>
+      </h1>
+
     </div>
   );
 }
